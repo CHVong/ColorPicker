5f70cff4d33590fba85e888079e05fc97e74888c
CV <brunchwithyou@gmail.com>
2022-05-23 01:17:05 -0700

    trying a quick fix for height reset using js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+function resetHeight(){
+    // reset the body height to that of the inner browser
+    document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
+
+
+
 // Variables
 let box1 = document.querySelector('.box1');
 let box2 = document.querySelector('.box2');
