b3da6b87848f5f7456c49a512181b08bc9ae6d83
CV <brunchwithyou@gmail.com>
2022-11-20 19:16:54 -0800

    added to do list for features

--- main.js
+++ main.js
@@ -59,3 +59,20 @@ saveButton.addEventListener('click',save)
 function save(){
     orderedList.innerHTML += `<li>${box1.style.backgroundColor} ${box2.style.backgroundColor}</li>`
 }
+
+
+//To do
+
+//page load random color
+
+//transparent box when click to copy or copied text centered and fade after 1 sec
+
+//generate background of color on clicked copied
+
+//saved color list to have 2 boxes
+
+//saved colors box will generate background on click
+
+//delete button for saved colors list
+
+//local storage to persist saved colors
