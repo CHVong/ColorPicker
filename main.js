2bd7ac30e7c3c83b807282360728da61c2687f31
CV <brunchwithyou@gmail.com>
2022-12-01 21:40:37 -0800

    added feature to save box set of colors.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let saveButton = document.querySelector('.save-button');
 let body = document.querySelector('body');
 let sidebarButton = document.querySelector('.sidebar-button')
 let sideContainer = document.querySelector('.sidecontainer')
-let orderedList = document.querySelector('.orderedList')
+let savedBoxContainer = document.querySelector('.savedbox-container')
 
 // Utility Functions
 function random(number){ //generate random number +1 at end to include max param
@@ -50,7 +50,7 @@ function openSidebar (){ //show/hide and changes different icon for sidebar
         sideContainer.style.right = '-15%'
         sidebarButton.innerHTML = `<i class="fa-solid fa-angles-left"></i>`
     }
-    
+
 }
 
 //Copying color on click and generate full page background color change
@@ -76,15 +76,32 @@ function copyColor(){
 
     setTimeout(function () {
         document.querySelector(`.${clickedBox}`).firstElementChild.classList.remove('show')
-    }, 250);
+    }, 200);
 }
 
 //Saving colors
 
-saveButton.addEventListener('click',save)
+saveButton.addEventListener('click', save)
 
 function save(){
-    orderedList.innerHTML += `<li>${box1.style.backgroundColor} ${box2.style.backgroundColor}</li>`
+    
+    if(document.querySelectorAll('.savedbox-container').length===5){
+       alert(`TOO many colors saved! Delete a set to save again.`)
+    }
+
+    //insertAdjacentHTML will make event listener persist. Do not use innerHTML to append otherwise eventlistener will not run.
+    if(document.querySelectorAll('.savedbox-container').length<5){
+        sideContainer.insertAdjacentHTML("beforeend", `<div class="savedbox-container">
+        <div class="deletebox"><i class="fa-solid fa-trash-can"></i></div>
+        <div class="savedbox"></div>
+        <div class="savedbox"></div>
+    </div>`);
+        
+    }
+
+
+
+// `<li>${box1.style.backgroundColor} ${box2.style.backgroundColor}</li>`
 }
 
 
